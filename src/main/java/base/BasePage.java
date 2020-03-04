98895cee0e8a3a1e832f9d3a153d831fb83160a6
DPazykov <pazikovd@gmail.com>
2020-03-04 08:17:30 +0200

    new

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,13 +1,31 @@
 package base;
 
 import manager.DriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+import static org.testng.AssertJUnit.assertTrue;
+
+public class BasePage extends LoadableComponent {
+
     public final WebDriverWait wait;
+    public WebDriver driver = DriverManager.getInstance().getDriver();
+    private String baseURL = "https://www.google.com.ua";
+
     public  BasePage(){
         PageFactory.initElements(DriverManager.getInstance().getDriver(), this);
         wait = new WebDriverWait(DriverManager.getInstance().getDriver(), 10);
+    }
+
+    @Override
+    protected void load() {
+        this.driver.get(baseURL);
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        assertTrue("HomePage is not loaded!", driver.getCurrentUrl().contains(baseURL));
     }
 }
